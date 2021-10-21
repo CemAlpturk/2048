2befd970ba62a9f5ce562ebd967a56037973e009
Cem Alpturk <cem.alpturk@gmail.com>
2021-10-21 14:02:19 +0200

    Ending conditions

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ class Game:
     def __init__(self, grid_size=4):
         self.grid_size = grid_size
         self.grid = np.zeros((self.grid_size, self.grid_size), dtype=int)
+        self.grid_prev = np.zeros((self.grid_size, self.grid_size), dtype=int)
         self.round = 0
         self.score = 0
         self.end = False
@@ -17,10 +18,10 @@ class Game:
     def add_random_tile(self):
         empty = np.argwhere(self.grid == 0)
         num_cells = empty.shape[0]
-        if num_cells != 0:
+        if num_cells != 0 and (self.grid != self.grid_prev).any():
             cell = empty[np.random.randint(num_cells)]
             self.grid[cell[0]][cell[1]] = 2
-        else:
+        elif self.check_moves():
             self.end = True
 
     def _move_left(self):
@@ -50,7 +51,7 @@ class Game:
                     if k > 1:
                         self.grid[i][j-k+1] = self.grid[i][j]
                         self.grid[i][j] = 0
-        self.add_random_tile()
+        # self.add_random_tile()
 
     def _move_right(self):
         for i in range(self.grid_size):
@@ -79,7 +80,7 @@ class Game:
                     if k > 1:
                         self.grid[i][j+k-1] = self.grid[i][j]
                         self.grid[i][j] = 0
-        self.add_random_tile()
+        # self.add_random_tile()
 
 
     def _move_up(self):
@@ -109,7 +110,7 @@ class Game:
                     if k > 1:
                         self.grid[i-k+1][j] = self.grid[i][j]
                         self.grid[i][j] = 0
-        self.add_random_tile()
+        # self.add_random_tile()
 
 
     def _move_down(self):
@@ -139,7 +140,7 @@ class Game:
                     if k > 1:
                         self.grid[i+k-1][j] = self.grid[i][j]
                         self.grid[i][j] = 0
-        self.add_random_tile()
+        # self.add_random_tile()
 
     def step(self, d):
         if not self.end:
@@ -152,8 +153,24 @@ class Game:
             else:
                 self._move_down()
             self.add_random_tile()
+            self.grid_prev = self.grid.copy()
         else:
             print("Game has ended")
+
+    def check_moves(self):
+        padded = np.zeros((self.grid_size+2, self.grid_size+2), dtype=int)
+        padded[1:self.grid_size+1, 1:self.grid_size+1] = self.grid  # Copy?
+        for i in range(1, self.grid_size+1):
+            for j in range(1, self.grid_size+1):
+                up = padded[i][j] == padded[i][j-1]
+                down = padded[i][j] == padded[i][j+1]
+                left = padded[i][j] == padded[i-1][j]
+                right = padded[i][j] == padded[i+1][j]
+
+                if up or down or left or right:
+                    return False
+        return True
+
 
 
 
